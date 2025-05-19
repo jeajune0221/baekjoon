612e490794ec2c75903bdd7f4973cb52706e09af
jeajune0221 <jeajune11117@gmail.com>
2025-05-19 22:15:09 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for (int i = n ; i >=1; i --){
+        for (int j = n ; j > n-i; j --) {
+            printf("*");
+        }
+        printf("\n");
+        for (int j = n; j >=i; j--)
+            printf(" ");
+    }
+    return 0;
+}

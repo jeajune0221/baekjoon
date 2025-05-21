1025c00daaaa1eeb473a83e3685cafe6173b6793
jeajune0221 <jeajune11117@gmail.com>
2025-05-21 09:47:43 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+    long long n1,n2,n3;
+    long long sum;
+    scanf("%lld %lld %lld",&n1,&n2,&n3);
+    sum=n1+n2+n3;
+    printf("%lld",sum);
+}
+

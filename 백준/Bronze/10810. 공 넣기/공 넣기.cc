bcb5c499afda7a79c1a4605cad14bc5987b16089
jeajune0221 <jeajune11117@gmail.com>
2025-06-04 17:47:08 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.cc
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -4,15 +4,15 @@
 int main() {
     int n,k;
     scanf("%d %d",&n,&k);
-    int *arr = (int*)malloc(sizeof(int)*n+1);
+    int *arr = (int*)malloc(sizeof(int)*n);
     for(int i = 1; i <= k; i++){
         int a,b,c;
         scanf("%d%d%d",&a,&b,&c);
-        for (int j = a; j <= b; j++){
+        for (int j = a-1; j < b; j++){
             arr[j] = c;
         }
     }
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
         printf("%d ",arr[i]);
     
     

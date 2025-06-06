6f5bafd4d50fa0dc456a052bb0e4d5661fc589f7
jeajune0221 <jeajune11117@gmail.com>
2025-06-06 14:28:16 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -5,11 +5,7 @@ int main(){
     for(int i = 0; i < 28; i++)
         scanf("%d",&arr[i]);
     for (int i = 0; i < 28; i++ ){
-        for (int j =1 ; j < 31; j++){
-            if (arr[i] == j)
-                check[j] = 1;
-                    }
-        
+        check[arr[i]] =1;
     }
             for(int i =1; i < 31; i++){
                 if (check[i] == 0)

28a0f18c76a1bd36bbd01f19e87b57f8d6242e71
jeajune0221 <jeajune11117@gmail.com>
2025-07-10 15:30:04 +0900

    [Silver V] Title: LCM, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/5347. LCM/LCM.cc
+++ 백준/Silver/5347. LCM/LCM.cc
@@ -1,6 +1,4 @@
-#include <iostream>
-using namespace std;
-
+#include<stdio.h>
 int gcd(int a, int b){
     if(b == 0)
         return a;
@@ -13,11 +11,12 @@ long long lcm(long long a, long long b){
 
 int main(void){
     int n;
-    cin >> n;
+    scanf("%d",&n);
     for(int i = 0; i < n; i++){
         long long a, b;
-        cin >> a >> b;
-        cout << lcm(a, b) << endl;
+        scanf("%lld %lld",&a,&b);
+        printf("%lld",lcm(a,b));
+        printf("\n");
     }
     return 0;
 }

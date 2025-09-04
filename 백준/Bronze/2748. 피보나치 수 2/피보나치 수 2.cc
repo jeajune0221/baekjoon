67df0ff4f92ff2d775bc16416f02aa6bb72c59d8
jeajune0221 <jeajune11117@gmail.com>
2025-09-05 08:28:56 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 1188 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -1,14 +1,15 @@
-#include <stdio.h>
-
-long long fibo(int n) {
-    static long long arr[91]={0,1,1};
+#include<stdio.h>
+long long arr[10000] = {0,1,1};
+long long fibo(long long n){
+    if(n<=1) return arr[n];
     if(arr[n]) return arr[n];
     arr[n] = fibo(n-1) + fibo(n-2);
+    
     return arr[n];
-
 }
-int main() {
-    int n;
+int main(void){
+    long long n;
     scanf("%d",&n);
     printf("%lld",fibo(n));
+    return 0;
 }

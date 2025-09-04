7455a506f7307c7a72118625a668b471e93c564b
jeajune0221 <jeajune11117@gmail.com>
2025-09-05 08:43:03 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -1,20 +1,17 @@
-#include <stdio.h>
-int main(void)
-{
-    int n, k;
-    int cont = 0, result = 0;
-    scanf("%d %d", &n, &k);
-    for (int i = 1; i <= n; i++)
-    {
-        if (n % i == 0)
+#include<stdio.h>
+int main(void){
+    int n,k;
+    int search=0;
+    scanf("%d %d",&n,&k);
+    int cont = 0;
+    for(int i = 1; i <=n; i++){
+        if(n%i == 0)
             cont++;
-        if (cont == k)
-        {
-            result = i;
-            break;
+        if(cont == k){
+            search = i;
+            printf("%d",search);
+            return 0;
         }
     }
-    printf("%d", result);
-
-    return 0;
+    printf("%d",0);
 }

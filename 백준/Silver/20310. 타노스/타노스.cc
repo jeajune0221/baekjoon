76d3c13c0be83ba77532cfcf5cb73097f55dc1d1
jeajune0221 <jeajune11117@gmail.com>
2025-09-10 10:47:36 +0900

    [Silver III] Title: 타노스, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.cc
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -1,27 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 int main(void)
 {
     char str[501];
     scanf("%s", str);
+    int len = 0;
     int cont0 = 0, cont1 = 0;
     for (int i = 0; str[i] != '\0'; i++)
     {
         if (str[i] == '0')
-        {
             cont0++;
+        else
+            cont1++;
+        len++;
+    }
+    int k = 0;
+    int half0 = cont0 / 2;
+    int half1 = cont1 / 2;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+
+        if (str[i] != '0')
+        {
+            str[i] = -1;
+            k++;
+        }
+        if (k == half1)
+        {
+            break;
+        }
+    }
+    k = 0;
+    for (int i = len - 1; i >= 0; i--)
+    {
+
+        if (str[i] == '0')
+        {
+            str[i] = -1;
+            k++;
+        }
+        if (k == half0)
+        {
+            break;
+        }
+    }
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == -1)
+        {
+            continue;
         }
         else
         {
-            cont1++;
+            printf("%c", str[i]);
         }
     }
-    for (int i = 0; i < cont0 / 2; i++)
-    {
-        printf("0");
-    }
-    for (int i = 0; i < cont1 / 2; i++)
-    {
-        printf("1");
-    }
+
     return 0;
 }

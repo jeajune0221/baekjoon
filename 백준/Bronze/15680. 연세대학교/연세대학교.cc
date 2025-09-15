53bbb15037604b9eb467069328697d6c68dcf24a
jeajune0221 <jeajune11117@gmail.com>
2025-09-15 23:27:13 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+    int n;
+    scanf("%d",&n);
+    if(n){
+        printf("Leading the Way to the Future");
+    }
+    else{
+        printf("YONSEI");
+    }
+    return 0;
+}

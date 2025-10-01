03031765f86279c86fb0c9311fd76da00245f8f4
jeajune0221 <jeajune11117@gmail.com>
2025-10-01 10:20:11 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    int a,b;
+    while(1){
+        scanf("%d %d",&a,&b);
+        if(a==0){
+            printf("비와이");
+            return 0;
+        }
+    }
+}

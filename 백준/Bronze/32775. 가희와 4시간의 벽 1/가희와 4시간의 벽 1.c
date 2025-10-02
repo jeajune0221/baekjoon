bdebcb79cb6fcf8298a5bdd4838848e77cec3dc3
jeajune0221 <jeajune11117@gmail.com>
2025-10-02 22:22:17 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void){
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a<=b){
+        printf("high speed rail");
+    }
+    else{
+        printf("flight");
+    }
+    return 0;
+        
+
+}

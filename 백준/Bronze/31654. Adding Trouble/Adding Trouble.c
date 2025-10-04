2111a6d313871505490467fdd975925153e248ea
jeajune0221 <jeajune11117@gmail.com>
2025-10-04 20:23:29 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a+b == c){
+        printf("correct!");
+    }
+    else{
+        printf("wrong!");
+    }
+    return 0;
+}

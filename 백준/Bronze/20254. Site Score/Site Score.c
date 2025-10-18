80e25eeda76d06d1e7bdf6c4335f6cd92ea684a9
jeajune0221 <jeajune11117@gmail.com>
2025-10-18 13:34:34 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.c
@@ -0,0 +1,8 @@
+//56UR + 24TR + 14UO + 6TO
+#include<stdio.h>
+int main(void){
+    int UR,TR,UO,TO;
+    scanf("%d %d %d %d",&UR,&TR,&UO,&TO);
+    printf("%lld",56*UR + 24*TR + 14*UO + 6*TO);
+    return 0;
+}

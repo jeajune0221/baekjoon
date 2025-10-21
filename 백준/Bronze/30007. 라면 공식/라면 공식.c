d19c596fac4455fedb2ad15999e9ac4af002ef33
jeajune0221 <jeajune11117@gmail.com>
2025-10-21 16:30:10 +0900

    [Bronze V] Title: 라면 공식, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++){
+        int a,b,x;
+        scanf("%d %d %d",&a,&b,&x);
+        printf("%d\n",a*(x-1) + b);
+    }
+    return 0;
+}

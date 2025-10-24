c7dbda8fe79cf7dddf02a84d1ecb6afb8dde1f5e
jeajune0221 <jeajune11117@gmail.com>
2025-10-25 01:27:42 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    int n;
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++){
+        int temp;
+        scanf("%d",&temp);
+        printf("%d %d\n",temp,temp);
+    }
+    return 0;
+}

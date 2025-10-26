4362b73e0c6ddaaaaaf34857e5e0d03dd78d01cc
jeajune0221 <jeajune11117@gmail.com>
2025-10-26 10:49:19 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n == 1)
+        printf("12 1600");
+    else if(n==2)
+        printf("11 894");
+    else if(n==3)
+        printf("11 1327");
+    else if(n==4)
+        printf("10 1311");
+    else if(n==5)
+        printf("9 1004");
+    else if(n==6)
+        printf("9 1178");
+    else if(n==7)
+        printf("9 1357");
+    else if(n==8)
+        printf("8 837");
+    else if(n==9)
+        printf("7 1055");
+    else if(n==10)
+        printf("6 556");
+    else
+        printf("6 773");
+    
+
+    return 0;
+}

20fc9cfb822f333223bae4f8e3018717e9879c26
jeajune0221 <jeajune11117@gmail.com>
2025-10-31 15:28:55 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,12 @@
+/*:fan::fan::fan:
+:fan::appa::fan:
+:fan::fan::fan:*/
+#include<stdio.h>
+int main(void){
+    char str[35];
+    scanf("%s",str);
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n",str);
+    printf(":fan::fan::fan:");
+    return 0;
+}

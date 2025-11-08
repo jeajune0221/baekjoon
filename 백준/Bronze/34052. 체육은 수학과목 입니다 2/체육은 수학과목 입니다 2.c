2edab49d339418fbf198e191bb9e17ba90638eff
jeajune0221 <jeajune11117@gmail.com>
2025-11-08 13:10:49 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&d);
+    if(a+b+c+d+300 <= 1800){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
